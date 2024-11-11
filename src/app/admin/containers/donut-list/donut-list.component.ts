2b68b6dbba94da0583b2a7e4c810738f13585948
Catherine Gaughan-Smith <catgsmith@users.noreply.github.com>
2024-11-11 14:41:26 +0000

    Add smart Component

---
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'donut-list',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>donut-list works!</p>`,
+  styles: ``,
+})
+export class DonutListComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DonutListComponent } from './admin/containers/donut-list/donut-list.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, DonutListComponent],
   selector: 'app-root',
   template: `
     <div class="app">
-      <header class="header">
-        <img src="/assets/img/logo.svg" alt="Ultimate Donuts" class="logo" />
-      </header>
+      <donut-list></donut-list>
     </div>
   `,
   styles: [
     `
         .app {
-            background: #fff;
-            border-radius: 8px;
-            max-width: 400px;
-            width: 94%;
-            margin: 25px auto;
-            padding: 25px;
-            border: 4px solid #ef9fc7;
+            margin-top: 50px;
+            font-size: 22px;
+            color: #fff;
+            text-align: center;
         }
-
-        .header {
-            display: flex;
-            justify-content: center;
-            margin-bottom: 25px;
-        }
-
-        .logo {
-            width: 100px;
-            height: 88px;
-        }
-    `
-  ]
+    `,
+  ],
 })
 export class AppComponent {
   title = 'angular-basics';
