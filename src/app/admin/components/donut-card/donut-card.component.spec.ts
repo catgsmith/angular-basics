588c9460150fb8593156eae338bc68db205d23fc
Catherine Gaughan-Smith <catgsmith@users.noreply.github.com>
2024-11-11 15:48:57 +0000

    fix unit test

--- src/app/admin/components/donut-card/donut-card.component.spec.ts
+++ src/app/admin/components/donut-card/donut-card.component.spec.ts
@@ -1,9 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DonutCardComponent } from './donut-card.component';
+import { Donut } from '../../models/donut.model';
+
 
 describe('DonutCardComponent', () => {
   let component: DonutCardComponent;
   let fixture: ComponentFixture<DonutCardComponent>;
+
+  const donut: Donut = {
+    id: '1',
+    name: 'Chocolate Donut',
+    icon: 'chocolate',
+    price: 250,
+    description: 'A delicious chocolate donut'
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,6 +22,7 @@ describe('DonutCardComponent', () => {
 
     fixture = TestBed.createComponent(DonutCardComponent);
     component = fixture.componentInstance;
+    component.donut = donut;  // Set the donut input here
     fixture.detectChanges();
   });
 
