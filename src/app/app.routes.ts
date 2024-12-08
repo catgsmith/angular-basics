9069e5e5f4c728fd3aa9b2a1dc6e1e68d2b8e5e5
Catherine Gaughan-Smith <catgsmith@users.noreply.github.com>
2024-12-08 20:05:37 +0000

    Lazy Loading Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,27 @@
 import { Routes } from '@angular/router';
-import { DonutListComponent } from './admin/containers/donut-list/donut-list.component';
-import { DonutSingleComponent } from './admin/containers/donut-single/donut-single.component';
 
 export const appRoutes: Routes = [
     {
         path: 'admin',
         children: [
-            { path: 'donuts', component: DonutListComponent },
-            { path: 'donut', component: DonutSingleComponent },
+            {
+                path: 'donuts',
+                loadComponent: () => import('./admin/containers/donut-list/donut-list.component').then(c => c.DonutListComponent)
+            },
+            {
+                path: 'donut',
+                loadComponent: () => import('./admin/containers/donut-single/donut-single.component').then(c => c.DonutSingleComponent)
+            },
             { path: '', pathMatch: 'full', redirectTo: 'donuts' },
         ],
     },
     {
         path: '',
         pathMatch: 'full',
+        redirectTo: 'admin',
+    },
+    {
+        path: '**',
         redirectTo: 'admin',
     },
 ];
